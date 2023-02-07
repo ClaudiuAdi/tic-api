1702c147ed2496bc52863f067c26a947c3e6a4a7
Claudiu <claudeady4579@gmail.com>
2023-02-07 22:13:55 +0200

    cors-commit

--- app.js
+++ app.js
@@ -14,38 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const origin = (origin, callback) => {
-  // allow requests with no origin or from dev environment
-  const isDev = process.env.NODE_ENV !== 'production';
-  if (!origin || isDev) {
-    return callback(null, true);
-  }
-
-  // allow requests from app URL
-  if (origin === process.env.APP_BASE_URL) {
-    return callback(null, true);
-  }
-
-  // allow request from whitelist
-  if (inWhitelist(origin)) {
-    return callback(null, true);
-  }
-
-  // deny any other request
-  callback(new Error('Not allowed by CORS'));
-};
-
-app.use(
-  cors({
-    origin,
-    credentials: true,
-    exposedHeaders: [
-      'Content-Disposition',
-      // add exposed headers here
-    ],
-  })
-);
-
 app.use(router);
 
 module.exports = app;
